f1a192c3429e41c4db0ba6d3d7b3a472c9d91735
romyr <romyrlopez123@gmail.com>
2026-03-20 17:13:07 +0800

    feat: finished object and date in scripts

--- lab5_scripts.js
+++ lab5_scripts.js
@@ -1,7 +1,77 @@
 //Object
+class Student {
+
+    constructor(studentName,age,email,course){
+        this.studentNumber = this.generateStudentNumber()
+        this.studentName = studentName
+        this.age = age
+        this.email = email
+        this.course = course
+
+        this.validateStudent()
+    }
+
+    generateStudentNumber(){
+        const additionalNumbers = new Set();
+
+        while (additionalNumbers.size < 5){
+            let digit = Math.floor(Math.random() * 10)
+            additionalNumbers.add(digit)
+        }
+        
+        console.log(additionalNumbers)
+        return('2024' + [...additionalNumbers].join(""))
+    }
 
 
 
+    validateStudent(){
+        if(!this.isValidName()){
+            throw new Error("Not valid Name")
+        }
+        if(!this.isValidAge()){
+            throw new Error("Not valid Age")
+        }
+
+        if(!this.isValidEmail()){
+            throw new Error("Not valid Email")
+        }
+        if(!this.isValidCourse()){
+            throw new Error("Not valid course")
+        }
+    }
+
+
+    isValidName(){
+        const regexPattern = /^[A-Za-z]+ [A-Za-z]+$/     //Regex Pattern from a to Z with one space can allow -
+        return regexPattern.test(this.studentName) && this.studentName.length >= 5;
+    }
+
+    isValidEmail(){
+        const regexPattern = /^[^\s@]+@up\.edu\.ph$/
+        return regexPattern.test(this.email)
+    }
+
+    isValidAge(){
+        if(this.age > 18 && this.age < 99){
+            return true
+        }
+        return false
+    }
+    
+    isValidCourse(Course){
+        const validCourses = [
+        "BA Food Appreciation",
+        "BA Applied Poetry of the Future",
+        "BS Computer Repair Shop",
+        "BS Video Gaming",
+        "BS Installing and Downloading"
+        ];
+
+        return validCourses.includes(this.course)
+    }
+
+}
 
 
 
@@ -41,5 +111,22 @@ function time_now(){
 
 }
 
+//Here put all students 
+Students = []
+
+
+
+//just checking if it works using node
+try {
+    const student3 = new Student(
+        "Juan Cruz",
+        20,
+        "juan@up.edu.ph",
+        "BS Video Gaming"
+    );
+    console.log(student3);
+} catch (error) {
+    console.log(error.message);
+}
 
 
